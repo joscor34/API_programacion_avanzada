0f8d9ac9ca7ac2819aff0c6bba3c6b45bec7e96c
José Esteva <zbrmr34@gmail.com>
2024-10-04 16:40:04 -0600

    Se agregó el archivo de pone a funcionar el servidor

---
+++ app.py
@@ -0,0 +1,15 @@
+from app import configurar_app
+
+# Este archivo se va a encargar de poner a correr el servidor
+
+# Aquí solamente se va a ejecutar el comando necesario para ponerlo a correr
+configurar_app().run(port=5000)
+# Inicializador <- Contructor
+ 
+# port -> Puerta
+
+# 22 -> ssh
+# 443 -> https
+# 80 -> http
+# 23 -> telnet
+# 8080 -> tu usuario va a poder acceder al servidor 
